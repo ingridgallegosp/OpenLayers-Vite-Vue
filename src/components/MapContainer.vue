3187e8e9fccd0511870d8f411c62b4736e3a1aab
Ingrid G <ing.gape18@gmail.com>
2023-05-08 17:43:09 -0500

    ya funcionan falta dar estilos

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -5,8 +5,12 @@
     </div>
     <br/>
     <div>
-        <input type='text' placeholder='ingresa coordenada e.g. 116.3970977, 39.9207883'/>
-        <button>Mostrar en Mapa</button>
+        <label>Añadir coordenada: </label>
+        <input @keydown.enter='agregarCoord' 
+        type='text' 
+        placeholder='e.g. 116.3970977, 39.9207883' 
+        v-model='inputContent'/>
+        <pre>{{ inputContent }}</pre>
     </div>
   
 </template>
@@ -32,16 +36,7 @@ import { toStringHDMS } from 'ol/coordinate.js';
 // Contenedor donde se va a imprimir
 const mapRoot = ref(null);
 
-// -2 referencia inicial para grupo de marcadores
-const referenciaMarcadores = ref([
-    { name: 'China Life Tower', coordenadas: [116.3970977, 39.9207883] },
-    { name: 'Zhengyangmen', coordenadas: [116.3827144, 39.9041912] },
-    { name: 'Ciudad Prohibida', coordenadas: [116.3827144, 39.9041912] },
-    { name: 'Templo del Cielo', coordenadas: [116.3960498, 39.887287] },
-]);
-console.log(referenciaMarcadores.value[0].name)
-const arrayP = referenciaMarcadores
-console.log(arrayP.value)
+/* Para marcador unico 
 
 // Crear marcador
 const marcador2 = new Feature({
@@ -49,8 +44,6 @@ const marcador2 = new Feature({
     name: 'Beijing',
     additionalInfo: 'Capital de China',
 });
-
-
 
 // Estilo para el marcador
 const iconStyle = new Style({
@@ -64,38 +57,60 @@ const iconStyle = new Style({
 
 marcador2.setStyle(iconStyle); 
 
-
 // Agregamos el marcador al arreglo
 const vectorSource = new VectorSource({
   features: [ marcador2], // A la capa le ponemos los marcadores
 });
 
-// -2
-const arrayMarcadores = () => arrayP.value.map((element, i) => {
-    element => {
-        // -2  grupo de marcadores
-        const crearMarcador = new Feature({
-            geometry: new Point(fromLonLat(element.value[i].coordenadas)),// En dónde se va a ubicar
-            name: element.value[i].name,
-            //additionalInfo: element.value[i].additionalInfo,
-        });
-        //console.log(crearMarcador)
-
-        return crearMarcador
-    }
-});
-console.log(arrayMarcadores());
-//crearMarcadores.setStyle(iconStyle); 
-
-
-const vectorSourceMarcadores = new VectorSource({
-  features: arrayMarcadores, // A la capa le ponemos los marcadores
-}); 
-
-// Layer marcadores
+// Layer marcador
 const vectorLayer = new VectorLayer({
   source: vectorSource,
 });
+*/
+
+// para grupo de Marcadres
+const inputContent = ref('')
+
+const referenciaMarcadores = ref([
+    { id: 1, name: 'China Life Tower', coordenadas: [116.3970977, 39.9207883] },
+    { id: 2, name: 'Zhengyangmen', coordenadas: [116.3827144, 39.9041912] },
+    { id: 3, name: 'Ciudad Prohibida', coordenadas: [116.3827144, 39.9041912] },
+    { id: 4, name: 'Templo del Cielo', coordenadas: [116.3960498, 39.887287] },
+]);
+//console.log(referenciaMarcadores.value)
+
+
+// Estilo para el marcador
+const iconStyle = new Style({
+    image: new Icon({
+        color: '#BADA55',
+        crossOrigin: 'anonymous',
+        src: "src/assets/location.png",
+        scale: 0.1,
+    })
+  }); 
+
+//marcador2.setStyle(iconStyle); falta dar estilos a cada marcador
+
+// Grupo de marcadores
+const arrayMarcadores = () => referenciaMarcadores.value.map((element) => {
+    const crearMarcador = new Feature({
+        geometry: new Point(fromLonLat(element.coordenadas)),// En dónde se va a ubicar
+        name: element.name,
+        //additionalInfo: element.additionalInfo,
+    });
+    return crearMarcador;
+});
+
+// Marcadores al arreglo / features es un arreglo
+const vectorSourceMarcadores = new VectorSource({
+  features: arrayMarcadores(), // A la capa le ponemos los marcadores
+}); 
+
+const vectorLayerMarcadores = new VectorLayer({
+  source: vectorSourceMarcadores,
+});
+
 
 // Layer mapa
 const rasterLayer = new TileLayer({
@@ -115,12 +130,27 @@ onMounted(() => {
     new Map({
         // el mapa se va a crear usando el template ref 'mapRoot'
         target: mapRoot.value,
-        layers: [rasterLayer, vectorLayer],
+        layers: [rasterLayer, vectorLayerMarcadores],
         view: new View({
             center: fromLonLat([116.390903, 39.904835]),
             zoom: 12, //menos zoom se aleja, mas zoom se acerca
         }),
     })
-})
+});
+
+//Logica de input
+const agregarCoord = () => {
+    referenciaMarcadores.value.push(
+        {
+            id: referenciaMarcadores.value.length,
+            name: '',
+            coordenadas: [inputContent.value]
+        }
+    )
+    inputContent.value= ''
+};
+
+
 </script>
+
 
